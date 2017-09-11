129ca6e6b9aed9b4c3040976838d6920fef78c26
aahel <aahel1guha@gmail.com>
2017-09-11 18:15:41 +0530

    lunar lander

---
+++ LunarLander17wi/app/src/main/java/cs193a/stanford/edu/lunarlander/LanderActivity.java
@@ -0,0 +1,28 @@
+/*
+ * CS 193A, Marty Stepp
+ * This activity is just a container that displays our lunar lander game and start/stops it.
+ */
+
+package cs193a.stanford.edu.lunarlander;
+
+import android.os.Bundle;
+import android.view.View;
+import stanford.androidlib.*;
+
+public class LanderActivity extends SimpleActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lander);
+    }
+
+    public void playClick(View view) {
+        LanderCanvas canvas = (LanderCanvas) findViewById(R.id.mycanvas);
+        canvas.startGame();
+    }
+
+    public void stopClick(View view) {
+        LanderCanvas canvas = (LanderCanvas) findViewById(R.id.mycanvas);
+        canvas.stopGame();
+    }
+}
